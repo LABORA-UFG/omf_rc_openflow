94c616fa86c7988a9935cb039122c7f4d7c292ac
Vinícius Gonçalves Braga <vinicius.esufg@gmail.com>
2016-12-07 14:45:17 -0200

    fixes to execute flowvisor RC only

---
+++ lib/flowvisor_rc.rb
@@ -0,0 +1,5 @@
+require "omf_rc_openflow/version"
+
+require 'omf_rc/resource_proxy/flowvisor_factory.rb'
+require 'omf_rc/resource_proxy/flowvisor.rb'
+require 'omf_rc/util/openflow_slice_tools.rb'
